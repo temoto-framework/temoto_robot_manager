8907a78e21aea494e09594f9f57e7cff3089f02d
FabianPG11 <parbian_11@hotmail.com>
2019-11-07 06:29:54 -0500

    Include urdf_loader file

---
+++ scripts/urdf_loader.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+import xacro
+import sys
+from cStringIO import StringIO
+
+def myhook():
+      print("Removing robot_description from parameter server")
+      if rospy.has_param("robot_description"):
+        rospy.delete_param("robot_description")
+
+rospy.on_shutdown(myhook)
+
+def loader():
+    rospy.init_node('urdf_loader', anonymous=True)
+
+    print("Parsing xacro: ", sys.argv[1])
+    sys.argv.insert(1, "--inorder")
+    sys.stdout = xacro_out = StringIO()
+    xacro.main()
+
+    
+    sys.stdout = sys.__stdout__
+#    print(xacro_out.getvalue())
+    rospy.set_param('robot_description', xacro_out.getvalue())
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        loader()
+    except rospy.ROSInterruptException:
+        pass 
