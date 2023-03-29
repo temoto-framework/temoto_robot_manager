519dd4ac62a0f0388ea7d3dd1650bdff85f20bf5
FabianEP11 <fabianparra@utexas.edu>
2023-03-29 10:25:43 -0500

    arguments

--- scripts/urdf_loader.py
+++ scripts/urdf_loader.py
@@ -19,21 +19,36 @@ rospy.on_shutdown(myhook)
 def loader():
     rospy.init_node('urdf_loader', anonymous=True)
 
+    if (len(sys.argv) == 3):
+        robot_description = sys.argv[2]
+    else:
+        robot_description = "robot_description"
+    
+    print(" *************** ++++++++++ ************ +++++++++++ **********")
+    print(sys.argv)
+    print(len(sys.argv))
+    print(sys.argv[2])              # ns of robot_description
+    sys.argv.pop(2)
+    print(len(sys.argv))
+
     print("Parsing xacro: ", sys.argv[1])
     sys.argv.insert(1, "--inorder")
+    print("After insert")
     sys.stdout = xacro_out = StringIO()
+    print("stdout insert")
     xacro.main()
+    print("After xacro main")
 
     
     sys.stdout = sys.__stdout__
 #    print(xacro_out.getvalue())
+    print("After sys stdout")
     
-    print(sys.argv[2])              # ns of robot_description
-    print(sys.argv[2]+'/robot_description')
+    # print(sys.argv[2]+'/robot_description')
 
     # rospy.set_param('robot_description', xacro_out.getvalue())
-    rospy.set_param('spot_arm/robot_description', xacro_out.getvalue())
-
+    rospy.set_param(robot_description, xacro_out.getvalue())
+    print("After set param")
 
     rospy.spin()
 
